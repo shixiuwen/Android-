f89bce94f181c100abbca30781f542ea45018961
shixiuwen <shixiuwen1991@yeah.net>
2016-09-02 15:25:03 +0800

    Update 实用代码.java

--- 实用代码.java
+++ 实用代码.java
@@ -11,4 +11,16 @@
     /**
      * 除了performClick之外的另一种模拟点击
      * */
+      public void onClickRD() {
+            emulateClick(mBt_konw);
+        }
+
+    private void emulateClick(View view) {
+        view.dispatchTouchEvent(MotionEvent
+                .obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(),
+                        MotionEvent.ACTION_DOWN, view.getLeft() + 5, view.getTop() + 5, 0));
+        view.dispatchTouchEvent(MotionEvent
+                .obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(),
+                        MotionEvent.ACTION_UP, view.getLeft() + 5, view.getTop() + 5, 0));
+    }
     
