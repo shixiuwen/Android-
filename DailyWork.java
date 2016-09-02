9422cab75c4b8ccb825bbda2f87d5b420fc45950
shixiuwen <shixiuwen1991@yeah.net>
2016-09-02 17:34:08 +0800

    Create DailyWork.java

---
+++ DailyWork.java
@@ -0,0 +1,9 @@
+2016.09.02
+
+ (Gank.io)：
+1.TextSwither                                  
+2.intent.resolveActivity(getPakageManager)     
+3.LitOrm(数据库操作)
+4.Android M 运行时权限检查(继承接口得到检查结果回调)
+5.onPostCreate()  (Activity完全创建后调用)
+6.PhotoViewAttacher(图片点击旋转长按事件)
