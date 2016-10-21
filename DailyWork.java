07cb69f53521799c06068bd118bad3f3bf9bf731
ShiXiuwen <shixiuwen1991@yeah.net>
2016-10-21 16:13:53 +0800

    Update DailyWork.java

--- DailyWork.java
+++ DailyWork.java
@@ -7,3 +7,12 @@
 4.Android M 运行时权限检查(继承接口得到检查结果回调)
 5.onPostCreate()  (Activity完全创建后调用)
 6.PhotoViewAttacher(图片点击旋转长按事件)
+ 
+ 
+2016.10.21
+ 1.webView方法：向下翻滚或者向上翻滚，回到顶部（底部）
+ pageUp(boolean top)
+ pageDown(boolean bottom)
+ 
+
+
