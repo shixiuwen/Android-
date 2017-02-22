f974f27b876fbd7c3a404c2524dd34af3dbbb988
ShiXiuwen <shixiuwen1991@yeah.net>
2017-02-22 15:51:41 +0800

    2017.02.22(有用但是未使用过的类)
    2017.02.22(有用但是未使用过的类)

--- DailyWork.java
+++ DailyWork.java
@@ -14,5 +14,11 @@
  pageUp(boolean top)
  pageDown(boolean bottom)
  
+2017.02.22(有用但是未使用过的类)
+ 1.BigDecimal
+ 2.MathContext
+ 3.CountDownTimer（Android中倒计时的正确使用姿势）
+ 4.AtomicInteger（一个提供原子操作的Integer的类。在Java语言中，++i和i++操作并不是线程安全的，在使用的时候，不可避免的会用到synchronized关键字。而AtomicInteger则通过一种线程安全的加减操作接口。）
+ 
 
 
